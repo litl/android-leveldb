925306e213212ff25305786bf9c362f59cf98ed5
Johan Bilien <jobi@litl.com>
2013-09-05 09:52:20 -0400

    Add a warning if resources are leaked.
    Print a warning if a NativeObject is finalized before its native
    resource is closed

--- library/src/com/litl/leveldb/NativeObject.java
+++ library/src/com/litl/leveldb/NativeObject.java
@@ -3,7 +3,10 @@ package com.litl.leveldb;
 import java.io.Closeable;
 import java.util.concurrent.atomic.AtomicInteger;
 
+import android.util.Log;
+
 abstract class NativeObject implements Closeable {
+    private static final String TAG = NativeObject.class.getSimpleName();
     protected long mPtr;
     private AtomicInteger mRefCount = new AtomicInteger();
 
@@ -49,5 +52,17 @@ abstract class NativeObject implements Closeable {
     @Override
     public void close() {
         unref();
+    }
+
+    @Override
+    protected void finalize() throws Throwable {
+        if (mPtr != 0) {
+            Log.w(TAG,
+                    "NativeObject "
+                            + getClass().getSimpleName()
+                            + " was finalized before native resource was closed, did you forget to call close()?");
+        }
+
+        super.finalize();
     }
 }
